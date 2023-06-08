563aba1e3e241d54417551f6f21cae4249b34745
Yoichiro Hara <hara@hacci.net>
2023-06-08 17:18:58 +0900

    ローダー追加

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-var params = {
-	container: document.getElementById('loader'),
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: "json/loader.json"
-};
- 
-var anim = lottie.loadAnimation(params);
